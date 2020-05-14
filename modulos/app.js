ea8aafdf70a8453090baf39599de0f12773c809f
Amanda Santana <asantana@thoughtworks.com>
2020-05-14 13:38:54 -0300

    Export custom functions

--- modulos/app.js
+++ modulos/app.js
@@ -1,5 +1,6 @@
 // import * as R from 'ramda';
 import { union as joinTogether, uniq } from 'ramda';
+import sumDefault, {sub, mult as multip, divisor, PI} from './utils';
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
 const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
@@ -9,3 +10,9 @@ const arr4 = uniq(arr2);
 
 console.log(arr3);
 console.log(arr4);
+
+console.log(sumDefault(3,2));
+console.log(sub(3,2));
+console.log(multip(3,2));
+console.log(divisor(4,2));
+console.log(PI);
